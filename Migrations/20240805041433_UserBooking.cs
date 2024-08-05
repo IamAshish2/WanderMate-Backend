3b4c98126f45420305baca4b26cb7874ec76a4e5
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-05 10:03:34 +0545

    migration added and database updated

---
+++ Migrations/20240805041433_UserBooking.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace secondProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Hotels_HotelId",
+                table: "Reviews");
+
+            migrationBuilder.RenameColumn(
+                name: "comment",
+                table: "Reviews",
+                newName: "Comment");
+
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Hotels",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "address",
+                table: "Hotels",
+                newName: "Address");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "Reviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    HotelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookingId);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Bookings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_userId",
+                table: "Reviews",
+                column: "userId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_HotelId",
+                table: "Bookings",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Hotels_HotelId",
+                table: "Reviews",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_userId",
+                table: "Reviews",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Hotels_HotelId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_userId",
+                table: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_userId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Reviews");
+
+            migrationBuilder.RenameColumn(
+                name: "Comment",
+                table: "Reviews",
+                newName: "comment");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Hotels",
+                newName: "price");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Hotels",
+                newName: "address");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Hotels_HotelId",
+                table: "Reviews",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
