9d16526535c555efab89b5b6935c15735b421713
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-09 11:56:36 +0545

    added review dtos

---
+++ Dtos/ReviewDTOs/GetReviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace secondProject.Dtos
+{
+    public class GetReviewDTO
+    {
+        public int Id { get; set; }
+        public int? Rating { get; set; }
+        public string? Comment { get; set; }
+    }
+}
---
+++ Dtos/ReviewDTOs/ReviewDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace secondProject.Dtos
+{
+    public class ReviewDto
+    {
+        public int? Rating{get; set;}
+        public string? Comment { get; set; }
+        public int? HotelId { get; set; }
+        public int? UserId { get; set; }
+    }
+}
