f88589f22235430338145616d66455a33554e606
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-12 22:48:16 +0545

    authentication setup

---
+++ Controller/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using secondProject.context;
+using secondProject.Dtos.UserDTOs;
+
+namespace secondProject.Controller
+{
+     [ApiController]
+     [Route("api/[Controller]")]
+    public class AuthenticationController:ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthenticationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginUser([FromBody] LoginDto login)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.UserName == login.UserName);
+            if (user == null) return BadRequest("The username does not exist.");
+
+            bool isPassValid = BCrypt.Net.BCrypt.Verify(login.Password, user.Password);
+            if (!isPassValid) return BadRequest("The password does not match.");
+
+            return Ok("Login Successful.");
+        }
+
+
+    }
+}
