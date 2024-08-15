419ea79ab0b3de1b8948ade73ed33bbe3638e415
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-15 12:34:31 +0545

    send token email to user for password reset

---
+++ Service/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using secondProject.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace secondProject.Service
+{
+    public class EmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient(_smtpSettings.Host)
+            {
+                Port = _smtpSettings.Port,
+                Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
+                EnableSsl = _smtpSettings.EnableSsl,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_smtpSettings.Username),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
