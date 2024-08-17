36e412b675fb57a6355d14ef7f02f3d47750d08c
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-17 14:28:06 +0545

    dtos defined for destinations model

---
+++ Dtos/DestinationDtos/GetDestinationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace secondProject.Dtos.DestinationDtos
+{
+    public class GetDestinationDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
+        public string? Description { get; set; }
+    }
+}
