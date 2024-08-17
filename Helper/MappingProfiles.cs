73c2ffe00b2415dc8c7193797ce5fbf11e9cc676
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-17 14:29:23 +0545

    map profiles with their dtos for map usuage in api endpoints

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using secondProject.Dtos.DestinationDtos;
+using secondProject.Dtos.ThingsToDoDtos;
+using secondProject.Models;
+
+
+namespace secondProject.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<ThingsToDo,ThingsToDoDto>();
+            CreateMap<ThingsToDoDto, ThingsToDo>();
+            CreateMap<Destination, GetDestinationDto>();
+            CreateMap<GetDestinationDto,Destination>();
+        }
+    }
+}
