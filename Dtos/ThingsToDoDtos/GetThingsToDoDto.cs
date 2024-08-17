7bdead2db9d5f80b47162625b0b229df6dc33711
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-17 14:28:37 +0545

    dtos defined for thingstodo model

---
+++ Dtos/ThingsToDoDtos/GetThingsToDoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace secondProject.Dtos.ThingsToDoDtos
+{
+    public class GetThingsToDoDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
+    }
+}
