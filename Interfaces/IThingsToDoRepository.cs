76a6ecfb94acaa5ab1d2cd0f0683218702421cfa
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-17 14:27:15 +0545

    repository pattern and api end point defined for thingstodo

---
+++ Interfaces/IThingsToDoRepository.cs
@@ -0,0 +1,16 @@
+﻿using secondProject.Models;
+
+namespace secondProject.Interfaces
+{
+    public interface IThingsToDoRepository
+    {
+        ICollection<ThingsToDo> GetThingsToDoList();
+        ThingsToDo GetThingsToDo(int id);
+        bool ThingsToDoExists(int thingsToDoId);
+        bool Save();
+        bool DeleteThingsToDo(ThingsToDo thingsToDo);
+        bool CreateThingsToDo(ThingsToDo  thingsToDo);
+        bool UpdateThingsToDo(ThingsToDo thingsToDo);
+        ThingsToDo searchByName(string name);
+    }
+}
---
+++ Repository/ThingsToDoRepository.cs
@@ -0,0 +1,60 @@
+﻿using secondProject.context;
+using secondProject.Interfaces;
+using secondProject.Models;
+
+namespace secondProject.Repository
+{
+    public class ThingsToDoRepository : IThingsToDoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ThingsToDoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool CreateThingsToDo(ThingsToDo thingsToDo)
+        {
+            _context.Add(thingsToDo);
+            return Save();
+        }
+
+        public bool DeleteThingsToDo(ThingsToDo thingsToDo)
+        {
+             _context.Remove(thingsToDo);
+            return Save();
+        }
+
+        public ThingsToDo GetThingsToDo(int id)
+        {
+            return _context.ThingsToDos.Where(t => t.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<ThingsToDo> GetThingsToDoList()
+        {
+            return _context.ThingsToDos.OrderBy(t => t.Id).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public ThingsToDo searchByName(string name)
+        {
+            var thingsToDo = _context.ThingsToDos.FirstOrDefault(t => t.Name == name);
+            return thingsToDo;
+        }
+
+        public bool ThingsToDoExists(int thingsToDoId)
+        {
+            return _context.ThingsToDos.Any(t => t.Id == thingsToDoId);
+        }
+
+        public bool UpdateThingsToDo(ThingsToDo thingsToDo)
+        {
+            _context.Update(thingsToDo);
+            return Save();
+        }
+    }
+}
